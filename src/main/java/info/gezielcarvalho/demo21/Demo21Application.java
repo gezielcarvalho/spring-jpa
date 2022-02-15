24ec8690e88ca439f26c53ad67e4865ba83f37e5
Geziel Carvalho <geziel.natal@gmail.com>
2022-02-15 08:10:22 -0300

    JSON Student Output

--- src/main/java/info/gezielcarvalho/demo21/Demo21Application.java
+++ src/main/java/info/gezielcarvalho/demo21/Demo21Application.java
@@ -1,10 +1,12 @@
 package info.gezielcarvalho.demo21;
 
+import info.gezielcarvalho.demo21.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
@@ -20,8 +22,17 @@ public class Demo21Application {
 //		return "Hello World";
 //	}
 
+//	@GetMapping
+//	public List<String> Hello(){
+//		return List.of("Hello", "World");
+//	}
+
 	@GetMapping
-	public List<String> Hello(){
-		return List.of("Hello", "World");
+	public List<Student> Hello(){
+		return List.of(
+				new Student(1L,"Geziel","gez@iel",49,LocalDate.of(1972,5,6)),
+				new Student(2L,"Josuel","jos@iel",49,LocalDate.of(1972,5,7)),
+				new Student(3L,"Jaziel","jaz@iel",49,LocalDate.of(1972,5,8))
+		);
 	}
 }
